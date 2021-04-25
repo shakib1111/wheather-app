958e3f6858ee4e01b06ffacca7479cde899ce7b3
shakib1111 <skshakib0000@gmail.com>
2021-04-25 16:32:31 +0530

    getting wheather info with city key

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,12 +1,35 @@
 const key='xmxgwJr6q6O1EpB7UtwJNAQhC9NJGRCc'
 
+//get wheather infromation
+
+const getWheather=async(id)=>{
+    const base='http://dataservice.accuweather.com/currentconditions/v1/'
+    const query=`${id}?apikey=${key}`;
+
+    const response= await fetch(base+query);
+    const data= await response.json();
+
+    return data[0];
+};
+
+
+
+
+// getting city information
 const getCity=async(city)=>{
     const base='http://dataservice.accuweather.com/locations/v1/cities/search'
     const query=`?apikey=${key}&q=${city}`
 
     const response= await fetch(base+query);
     const data = await response.json();
-    console.log(data);
+    return data[0];
 };
 
-getCity('hyderabad')
+getCity('mumbai')
+        .then(data=> {
+            return getWheather(data.Key)
+        }
+        ).then(data=>{
+            console.log(data)
+        })
+        .catch(err=> console.log(err));
