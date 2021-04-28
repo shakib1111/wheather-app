f1559b6189212a8ad3e13883f1395162a2bd73dc
shakib1111 <skshakib0000@gmail.com>
2021-04-28 16:50:29 +0530

    txt added

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,33 @@
 const cityForm=document.querySelector('form');
+const card=document.querySelector('.card');
+const details=document.querySelector('.details');
+
+
+const updatUI=(data)=>
+{
+    const cityDets=data.cityDets;
+    const wheather=data.wheather;
+
+    //update details template
+
+    details.innerHTML=`
+            <h5 class="my-3">${cityDets.EnglishName}</h5>
+            <div class="my-3">${wheather.weatherText}</div>
+            <div class="display-4 my-4">
+              <span>${wheather.Temperature.Metric.Value}</span>
+              <span>&deg;C</span>
+            </div>
+    
+    `
+};
+
+
 
 
 const updateCity=async(city)=>{
 
     const cityDets= await getCity(city);
-    const wheather= await getWheather(cityDets.Key);
+    const wheather= await getwheather(cityDets.Key);
 
     return {
         cityDets: cityDets,
@@ -21,5 +44,7 @@ cityForm.addEventListener('submit',e=>{
     cityForm.reset();
 
 
-    updateCity(city);
+    updateCity(city)
+    .then(data=> updatUI(data))
+    .catch(err=> console.log(err));
 });
